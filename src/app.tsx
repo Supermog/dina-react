15686da23b6e6b0c4a909da77ea6336fbff1cc43
Supermog <balint@gearedapp.co.uk>
2026-03-11 15:27:19 +0100

    feat: add tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h1>Vite + React</h1>
+        <div className="flex flex-col items-center justify-center gap-2">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
